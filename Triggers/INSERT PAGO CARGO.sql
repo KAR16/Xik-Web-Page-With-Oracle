d5b822cd61cb10c1a9d195b6907785ab7017e616
KAR16 <kherrera16@gmail.com>
2016-06-01 09:59:48 -0600

    Trigger 2 version quetzales listo

--- Triggers/INSERT PAGO CARGO.sql
+++ Triggers/INSERT PAGO CARGO.sql
@@ -9,9 +9,10 @@ FECHA_LIMITE DATE;
 --VARIABLES DE CALCULO
 TIPO_CAMBIO NUMERIC(11, 2) := (:NEW.TIPO_DE_CAMBIO);
 MONEDA INT := (:NEW.MONEDA);
-PAGO INT := (:NEW.MONTO);
+PAGO NUMERIC(11, 2) := (:NEW.MONTO);
 DOLLAR NUMERIC(11, 2);
 RESULTADO NUMERIC(11,2);
+CONT INT := 0;
 
 --CURSOR
 CURSOR CURCargoEstudiante IS
@@ -28,44 +29,101 @@ MES := (EXTRACT(MONTH FROM (:NEW.FECHA_APLICACION)));
 DIA:= (EXTRACT(DAY FROM (:NEW.FECHA_APLICACION)));
 
 DIA := DIA - 1; 
-
-FECHA_LIMITE := TO_DATE(AÑO || '/' || (MES + 1) || '/' || DIA, 'YYYY/MM/DD');
-
+MES := MES + 1;
+FECHA_LIMITE := TO_DATE(AÑO || '/' || MES || '/' || DIA, 'YYYY/MM/DD');
 
 
 FOR Iteracion IN CURCargoEstudiante
 LOOP
-DBMS_OUTPUT.put_line(Iteracion.FECHA_VENCIMIENTO);
-DBMS_OUTPUT.put_line(FECHA_LIMITE);
- IF (PAGO > 0) THEN
-    IF (Iteracion.FECHA_VENCIMIENTO <= FECHA_LIMITE AND Iteracion.MONEDA = MONEDA) THEN
-        DBMS_OUTPUT.put_line('Soy Menor que tu fecha');
-        IF (PAGO <= Iteracion.SALDO) THEN
-            RESULTADO := Iteracion.SALDO - PAGO;
-            --UPDATE ENTIDAD CARGO
-            UPDATE CARGO
-            SET SALDO = RESULTADO
-            WHERE CARGO = Iteracion.CARGO AND FECHA_VENCIMIENTO = Iteracion.FECHA_VENCIMIENTO;
+
+    DBMS_OUTPUT.put_line(Iteracion.FECHA_VENCIMIENTO);
+    DBMS_OUTPUT.put_line(FECHA_LIMITE);
+    IF (MONEDA = 1) THEN
+        DBMS_OUTPUT.put_line('ENTRE EN QUETZALES');
+        IF (PAGO > 0 ) THEN
+            IF (Iteracion.FECHA_VENCIMIENTO <= FECHA_LIMITE AND Iteracion.MONEDA = 1) THEN
+                DBMS_OUTPUT.put_line('ENTRE A PAGO > 0 Y TOCA PAGAR Q');
+                DBMS_OUTPUT.put_line('Soy Menor que tu fecha y entre en Quetzales');
+                IF (PAGO <= Iteracion.SALDO) THEN
+                    RESULTADO := Iteracion.SALDO - PAGO;
+                    --UPDATE ENTIDAD CARGO
+                    UPDATE CARGO
+                    SET SALDO = RESULTADO
+                    WHERE CARGO = Iteracion.CARGO AND FECHA_VENCIMIENTO = Iteracion.FECHA_VENCIMIENTO;
+                    DBMS_OUTPUT.put_line('UPDATE DE CARGO HECHO Q. / pago es menor 1');
+
+                    --INSERT ENTIDAD PAGO_CARGO
+                    INSERT INTO PAGO_CARGO
+                    VALUES(SECIdPagoCargo.NEXTVAL, :NEW.PAGO, Iteracion.CARGO, PAGO);
+                    DBMS_OUTPUT.put_line('INSERT EN PAGO_CARGO HECHO Q. pago es menor 1');
+                --SE RESTA EL PAGO
+                PAGO := PAGO - Iteracion.SALDO;
+                DBMS_OUTPUT.put_line(PAGO);
+                ELSE
+                    --UPDATE ENTIDAD CARGO
+                    UPDATE CARGO
+                    SET SALDO = 0
+                    WHERE CARGO = Iteracion.CARGO AND FECHA_VENCIMIENTO = Iteracion.FECHA_VENCIMIENTO;
+                    DBMS_OUTPUT.put_line('UPDATE DE CARGO HECHO Q. / pago es mayor 2');
+
+                    --INSERT ENTIDAD PAGO_CARGO
+                    INSERT INTO PAGO_CARGO
+                    VALUES(SECIdPagoCargo.NEXTVAL, :NEW.PAGO, Iteracion.CARGO, Iteracion.SALDO);
+                    DBMS_OUTPUT.put_line('INSERT EN PAGO_CARGO HECHO Q. / pago es mayor 2');
+                    --SE RESTA EL PAGO
+                PAGO := PAGO - Iteracion.SALDO;
+                DBMS_OUTPUT.put_line(PAGO);
+                END IF;
+            END IF;
+
             
-            --INSERT ENTIDAD PAGO_CARGO
-            INSERT INTO PAGO_CARGO
-            VALUES(SECIdPagoCargo.NEXTVAL, :NEW.PAGO, Iteracion.CARGO, PAGO);
+            IF (Iteracion.FECHA_VENCIMIENTO <= FECHA_LIMITE AND Iteracion.MONEDA = 2) THEN
+                IF (CONT = 0) THEN
+                    DBMS_OUTPUT.put_line('LOS Q. ' || PAGO);
+                    PAGO := PAGO / TIPO_CAMBIO;
+                    CONT := CONT +1;
+                    DBMS_OUTPUT.put_line('AHORA SON $. ' || PAGO);
+                END IF;
+                DBMS_OUTPUT.put_line('ENTRE A PAGO > 0 Y AHORA TOCA PAGAR $');
+                DBMS_OUTPUT.put_line('Soy Menor que tu fecha y entre en Dolares');
+                IF (PAGO <= Iteracion.SALDO) THEN
+                    RESULTADO := Iteracion.SALDO - PAGO;
+                    --UPDATE ENTIDAD CARGO
+                    UPDATE CARGO
+                    SET SALDO = RESULTADO
+                    WHERE CARGO = Iteracion.CARGO AND FECHA_VENCIMIENTO = Iteracion.FECHA_VENCIMIENTO;
+                    DBMS_OUTPUT.put_line('UPDATE DE CARGO HECHO $. / pago es menor 3');
+
+                    --INSERT ENTIDAD PAGO_CARGO
+                    INSERT INTO PAGO_CARGO
+                    VALUES(SECIdPagoCargo.NEXTVAL, :NEW.PAGO, Iteracion.CARGO, PAGO);
+                    DBMS_OUTPUT.put_line('INSERT EN PAGO_CARGO HECHO $. / pago es menor 3');
+                --SE RESTA EL PAGO
+                PAGO := PAGO - Iteracion.SALDO;
+                DBMS_OUTPUT.put_line(PAGO);
+                ELSE
+                    --UPDATE ENTIDAD CARGO
+                    UPDATE CARGO
+                    SET SALDO = 0
+                    WHERE CARGO = Iteracion.CARGO AND FECHA_VENCIMIENTO = Iteracion.FECHA_VENCIMIENTO;
+                    DBMS_OUTPUT.put_line('UPDATE DE CARGO HECHO $. / pago es mayor 4');
+                    
+                    --INSERT ENTIDAD PAGO_CARGO
+                    INSERT INTO PAGO_CARGO
+                    VALUES(SECIdPagoCargo.NEXTVAL, :NEW.PAGO, Iteracion.CARGO, Iteracion.SALDO);
+                    DBMS_OUTPUT.put_line('INSERT EN PAGO_CARGO HECHO $. / pago es mayor 4');
+
+                --SE RESTA EL PAGO
+                PAGO := PAGO - Iteracion.SALDO;
+                DBMS_OUTPUT.put_line(PAGO);
+                END IF;
+            END IF;
         ELSE
-            --UPDATE ENTIDAD CARGO
-            UPDATE CARGO
-            SET SALDO = 0
-            WHERE CARGO = Iteracion.CARGO AND FECHA_VENCIMIENTO = Iteracion.FECHA_VENCIMIENTO;
-            
-            --INSERT ENTIDAD PAGO_CARGO
-            INSERT INTO PAGO_CARGO
-            VALUES(SECIdPagoCargo.NEXTVAL, :NEW.PAGO, Iteracion.CARGO, Iteracion.SALDO);
+            DBMS_OUTPUT.put_line('Llegue al final Quetzales');
+            EXIT WHEN PAGO <= 0 OR (Iteracion.FECHA_VENCIMIENTO > FECHA_LIMITE);
         END IF;
     END IF;
-PAGO := PAGO - Iteracion.SALDO;
-ELSE
-    EXIT WHEN PAGO <= 0 OR Iteracion.FECHA_VENCIMIENTO > FECHA_LIMITE;
-END IF;
+    
 END LOOP;
--- FIN BEGIN 
   NULL;
 END;
