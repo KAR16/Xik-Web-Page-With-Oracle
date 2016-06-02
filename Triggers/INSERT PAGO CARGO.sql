8b65106786097ec76143beb816bef549b6b41beb
KAR16 <kherrera16@gmail.com>
2016-06-02 14:57:16 -0600

    Trigger 2 con arreglos de fecha completo

--- Triggers/INSERT PAGO CARGO.sql
+++ Triggers/INSERT PAGO CARGO.sql
@@ -5,6 +5,7 @@ DECLARE
 --VARIABLES DE FECHA
 DIA INT; MES INT; AÑO INT;
 FECHA_LIMITE DATE;
+AÑO_BIS VARCHAR(20);
 
 --VARIABLES DE CALCULO
 TIPO_CAMBIO NUMERIC(11, 2) := (:NEW.TIPO_DE_CAMBIO);
@@ -34,9 +35,45 @@ AÑO := (EXTRACT(YEAR FROM (:NEW.FECHA_APLICACION)));
 MES := (EXTRACT(MONTH FROM (:NEW.FECHA_APLICACION)));
 DIA:= (EXTRACT(DAY FROM (:NEW.FECHA_APLICACION)));
 
-DIA := DIA - 1; 
-MES := MES + 1;
-FECHA_LIMITE := TO_DATE(AÑO || '/' || MES || '/' || DIA, 'YYYY/MM/DD');
+--SELECT QUE EXTRAE SI EL AÑO ES BISIESTO O NO
+    SELECT decode(LAST_DAY(TO_date('01/02/' || AÑO ,'dd/mm/yyyy')), TO_DATE('28/02/'|| AÑO,'DD/MM/YYYY') , 'no_bisiesto', 'bisiesto')
+    INTO AÑO_BIS
+    FROM DUAL;
+  
+  IF (DIA = 1 AND MES = 1 AND AÑO_BIS = 'bisiesto')THEN
+      DIA := 29;
+      MES := MES + 1;
+  ELSIF (DIA = 1 AND MES = 1 AND AÑO_BIS = 'no_bisiesto')THEN
+      DIA := 28;
+      MES := MES + 1;
+  ELSIF (DIA = 1 AND MES = 12) THEN
+      DIA := 31;
+      MES := 1;
+      AÑO := AÑO + 1;
+  ELSIF (DIA > 1 AND MES = 12) THEN
+      DIA := DIA - 1;
+      MES := 1;
+      AÑO := AÑO + 1;
+  ELSIF (DIA = 1 AND (MES = 2 OR MES = 6 OR MES = 7 OR MES = 9 OR MES = 11)) THEN
+      DIA := 31;
+      MES := MES + 1;
+  ELSIF (DIA = 1 AND (MES = 3 OR MES = 4 OR MES = 5 OR MES = 8 OR MES = 10)) THEN
+      DIA := 30;
+      MES := MES + 1;
+  ELSE
+      DIA := DIA - 1; 
+      MES := MES + 1;
+  END IF;
+
+  FECHA_LIMITE := TO_DATE(AÑO || '/' || MES || '/' || DIA, 'YYYY/MM/DD');
+  
+  dbms_output.put_line('FECHA: ' || (:NEW.FECHA_APLICACION));
+  dbms_output.put_line('DIA: ' || DIA);
+  dbms_output.put_line('MES: ' || MES);
+  dbms_output.put_line('AÑO: ' || AÑO);
+  dbms_output.put_line('FECHA LIMITE: ' || FECHA_LIMITE);
+  
+  dbms_output.put_line('AÑO: ' || AÑO_BIS);
 
 IF (MONEDA = 1) THEN
         FOR Iteracion IN CURCargoEstudianteQuetzales
@@ -232,5 +269,9 @@ IF (MONEDA = 2) THEN
         END IF;
     END LOOP;
 END IF;
+EXCEPTION
+    /*Parte de excepciones*/
+    WHEN OTHERS THEN
+    dbms_output.put_line('Se ha producido un error');
   NULL;
 END;
